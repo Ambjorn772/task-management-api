2decd49fc98e6dfbba1e1b871c992272f938512b
Ambjorn772 <sinianskyi.arsenii@lll.kpi.ua>
2025-12-19 02:17:57 +0200

    chore: initial commit - Lab 0 completed

---
+++ index.js
@@ -0,0 +1,18 @@
+// Node.js проект
+console.log('Hello, Node.js!');
+
+// Пример простого сервера (раскомментируйте при необходимости)
+/*
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Привет из Node.js сервера!');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
+*/
+
